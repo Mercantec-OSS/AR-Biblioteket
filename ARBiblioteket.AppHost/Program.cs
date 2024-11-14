282aa9c8336c4383d50b7c50656e4cf81a8d9817
StineDk <144903306+StineDk@users.noreply.github.com>
2024-11-14 11:22:49 +0100

    atempt to fix gitignore problem

--- ARBiblioteket.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var apiService = builder.AddProject<Projects.ARBiblioteket_ApiService>("apiservice");
-
-builder.AddProject<Projects.ARBiblioteket_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(apiService);
-
-builder.Build().Run();

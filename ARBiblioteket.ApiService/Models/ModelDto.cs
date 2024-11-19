f8cdf14485a3265445e966fe0d265667a274cd1e
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2024-11-19 16:02:43 +0100

    Modles Controller

---
+++ ARBiblioteket.ApiService/Models/ModelDto.cs
@@ -0,0 +1,8 @@
+public class ModelDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ThreeDFile { get; set; } = string.Empty;
+    public string ImageFile { get; set; } = string.Empty;
+    public int UploaderId { get; set; }
+} 

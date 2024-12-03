6982ce3878d8ebc28b76ef3179383dd7412e86ac
Mape0009 <mape0009@edu.mercantec.dk>
2024-12-03 13:25:32 +0100

    DbConnection

--- ARBiblioteket.ApiService/Program.cs
+++ ARBiblioteket.ApiService/Program.cs
@@ -1,15 +1,23 @@
+using ARBiblioteket.ApiService.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Add service defaults & Aspire components.
 builder.AddServiceDefaults();
-
-// Add services to the container.
 builder.Services.AddProblemDetails();
 builder.Services.AddScoped<IModelService, ModelService>();
+
+// Register ApplicationDbContext with MySQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 40)) // Replace with your actual MySQL version
+    )
+);
 
 var app = builder.Build();
 
@@ -18,10 +26,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
-
---
+++ ARBiblioteket.ApiService/Services/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ARBiblioteket.ApiService.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Model> Models { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                _configuration.GetConnectionString("DefaultConnection"),
+                new MySqlServerVersion(new Version(8, 0, 40))
+            );
+        }
+    }
+}

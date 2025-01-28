04e3f8d1376d75f5c9106ed6539f2c017ce14f1a
Elic0 <charlieoljac@gmail.com>
2025-01-28 12:38:27 +0100

    minor changes

--- resources/views/viewmodel.blade.php
+++ resources/views/viewmodel.blade.php
@@ -6,13 +6,13 @@
 <div class="container">
     <div class="model-container">
         <h2 class="model-title">{{ $model->title }}</h2>
-        
+
         <!-- Model Image -->
-        <img src="{{ asset('storage/images/' . $model->image_path) }}" 
+        <img src="{{ asset('storage/images/' . $model->image_path) }}"
              alt="{{ $model->title }}" 
              class="model-image">
-        
-        <!-- Model Viewer -->
+
+        <!-- 3D Model Viewer -->
         <model-viewer 
             src="{{ asset('storage/models/' . $model->model_path) }}"
             alt="{{ $model->title }}"
@@ -26,7 +26,7 @@
         <div class="model-description">
             <p><strong>Education:</strong> {{ $model->education }}</p>
             <p><strong>Description:</strong> {{ $model->description }}</p>
-            <p><strong>Uploaded:</strong> {{ \Carbon\Carbon::parse($model->uploaded_at)->format('d/m/Y') }}</p>
+            <p><strong>Uploaded:</strong> {{ \Carbon\Carbon::parse($model->created_at)->format('d/m/Y') }}</p>
             <p><strong>Last Edited:</strong> {{ \Carbon\Carbon::parse($model->updated_at)->format('d/m/Y') }}</p>
         </div>
     </div>

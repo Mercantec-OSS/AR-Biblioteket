cbfd164d57fbbb2be1f9f4b8a703a86834de5120
Tobias <115145472+TobiasDK22@users.noreply.github.com>
2025-01-28 10:44:38 +0100

    Front page works
    Modeller bliver nu hentet fra databasen på forsiden.
    Filter på forsiden er også blevet tilføjet. Nu kan man filtrer modeller udfra uddannelser.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,55 +8,49 @@
         <div class="filter-section">
             <h3>Filtrer Modeller</h3>
             <div class="filter-content">
-                <p class="coming-soon">Kommer Snart!</p>
+                <select id="educationFilter" class="filter-select">
+                    <option value="">Alle uddannelser</option>
+                    <option value="Auto">Auto</option>
+                    <option value="Automatik">Automatik</option>
+                    <option value="Business">Business</option>
+                    <option value="Data">Data</option>
+                    <option value="Elektriker">Elektriker</option>
+                    <option value="Elektronik">Elektronik</option>
+                    <option value="Gastronomi">Gastronomi</option>
+                    <option value="Industriteknik">Industriteknik</option>
+                    <option value="Operatør">Operatør</option>
+                    <option value="Produktør">Produktør</option>
+                    <option value="Smed">Smed</option>
+                    <option value="Struktør">Struktør</option>
+                    <option value="Tømrer">Tømrer</option>
+                    <option value="VVS">VVS</option>
+                </select>
             </div>
         </div>
     </div>
     <div class="content">
         <ul class="model-list">
-            @php
-                $dummyModels = [ 
-                    [
-                        'id' => 1, 
-                        'title' => '3D Model 1',
-                        'education' => 'Elektriker'
-                    ],
-                    [
-                        'id' => 2, 
-                        'title' => '3D Model 2',
-                        'education' => 'VVS'
-                    ],
-                    [
-                        'id' => 3, 
-                        'title' => '3D Model 3',
-                        'education' => 'Gastronomi'
-                    ],
-                    [
-                        'id' => 4, 
-                        'title' => 'AR Model 1',
-                        'education' => 'Auto'
-                    ],
-                    [
-                        'id' => 5, 
-                        'title' => 'AR Model 2',
-                        'education' => 'Business'
-                    ],
-                ];
-            @endphp
-
-            @foreach($dummyModels as $index => $model)
+            @foreach($models as $index => $model)
                 <li class="model-item" style="--i: {{ $index }};">
-                    <a href="/model/{{ $model['id'] }}" class="model-link">
+                    <a href="/model/{{ $model->id }}" class="model-link">
                         <div>
-                            <span class="model-title">{{ $model['title'] }}</span>
+                            <span class="model-title">{{ $model->title }}</span>
                             <div style="color: #64748b; font-size: 0.9rem; margin-top: 0.25rem;">
-                                <span>{{ $model['education'] }}</span>
+                                <span>{{ $model->education }}</span>
                             </div>
                         </div>
                         <span class="arrow-icon">→</span>
                     </a>
                 </li>
             @endforeach
+
+            @if($models->isEmpty())
+                <li class="model-item">
+                    <div class="model-link">
+                        <span>Ingen modeller fundet</span>
+                    </div>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
